635c7944d9e7d7530e860718a201a5e34d78fda8
alex62320 <alex.62320@live.fr>
2022-03-02 11:58:18 +0100

    mise à jour cours variables
    exploration de convertion de type

--- variables.py
+++ variables.py
@@ -7,27 +7,34 @@
 
 nombre = 123
 print(nombre)
+print(type(nombre))
 
 nombre = 123.0
 print(nombre)
+print(type(nombre))
 
 nombre = 3.14
 print(nombre)
+print(type(nombre))
 
 nombre = "123"
 print(nombre)
+print(type(nombre))
 
 text = "lorem ipsum"
 print(text)
+print(type(text))
 
 is_day = True
 print(is_day)
+print(type(is_day))
 
 has_sugar = False
 print(has_sugar)
 
 has_accepted_ula = None
 print(has_accepted_ula)
+print(type(has_accepted_ula))
 
 html_code = '<h1>titre de mon blog</h1>'
 print(html_code)
@@ -45,3 +52,61 @@ bar
 baz
 """
 print(multiline_text)
+
+nombre = "5"
+print(nombre)
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
+
+nombre = "2.71"
+print(nombre)
+print(type(nombre))
+nombre = float(nombre)
+print(nombre)
+print(type(nombre))
+
+nombre = 3.14
+print(nombre)
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
+
+texte = str(nombre)
+print(texte)
+print(type(texte))
+
+my_var = 0
+my_var = bool(my_var)
+print(my_var)
+
+my_var = -123
+my_var = bool(my_var)
+print(my_var)
+
+my_var = -123
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "0"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = ""
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "123"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "-123"
+my_var = bool(my_var)
+print(my_var)
+
+
+my_var = [False]
+my_var = bool(my_var)
+print(my_var)
