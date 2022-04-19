6ec545491a8c3abb85693377370e16d0372e72b6
alex62320 <alex.62320@live.fr>
2022-04-19 14:24:36 +0200

    Ajout de l'exercie 10
    exercice 10 terminer

--- exercice-10-fonctions.py
+++ exercice-10-fonctions.py
@@ -22,7 +22,11 @@ print(my_sum(8, 7))
 # Appelez la fonction et affichez le résultat
 
 # réponse 10.2
+def my_diff(a,b) -> int:
+    soustract = b - a
+    return soustract
 
+print(my_diff(9, 8))
 # exo 10.3
 # Créer une fonction nommée `oui_non()` qui :
 # - prend un paramètre booléen
@@ -32,7 +36,14 @@ print(my_sum(8, 7))
 # Appelez la fonction avec la valeur False et affichez le résultat
 
 # réponse 10.3
-
+def oui_non(a) -> bool:
+    if a is True:
+        return 'Oui'
+    else:
+        return 'Non'
+    
+print(oui_non(True))
+print(oui_non(False))
 # exo 10.4
 # Créer une fonction nommée `is_greater()` qui :
 # - prend deux paramètres `a` et `b` de type `float`
@@ -41,7 +52,14 @@ print(my_sum(8, 7))
 # Appelez la fonction et affichez le résultat
 
 # réponse 10.4
+def is_greater(a, b) -> float:
+    if a > b :
+        return True
+    else:
+        return False
 
+print(is_greater(12.25, 25))
+print(is_greater(25, 12.25))
 # exo 10.5
 # Créer une fonction nommée `compare()` qui :
 # - prend deux paramètres `a` et `b` de type `float`
@@ -51,7 +69,17 @@ print(my_sum(8, 7))
 # Appelez la fonction et affichez le résultat
 
 # réponse 10.5
+def compare(a, b) -> float:
+    if a > b:
+        return 1
+    elif a < b:
+        return -1
+    else:
+        return 0
 
+print(compare(11, 10))
+print(compare(10,11))
+print(compare(10, 10))
 # exo 10.6
 # La formule suivante permet de convertir des mètres en miles :
 #
@@ -74,6 +102,17 @@ print(my_sum(8, 7))
 # Appelez les fonctions et affichez les résultats
 
 # réponse 10.6
+def meters_to_miles(meters) -> float:
+    miles= meters / 1609.344
+    return f'{miles} miles'
+
+print(meters_to_miles(1000))
+
+def miles_to_meters(miles)-> float:
+    meters= miles * 1609.344
+    return f'{meters} meters'
+
+print(miles_to_meters(10))
 
 # exo 10.7
 # Créer une fonction nommée `compute_tax()` qui :
@@ -89,4 +128,21 @@ print(my_sum(8, 7))
 # Référence : [Quels sont les taux de TVA en vigueur en France et dans l'Union européenne ? | economie.gouv.fr](https://www.economie.gouv.fr/cedef/taux-tva-france-et-union-europeenne)
 
 # réponse 10.7
+def compute_tax(price: float,tax_type: int):
+    if tax_type == 1:
+        return price * 1.021
+    elif tax_type == 2:
+        return price * 1.055
+    elif tax_type == 3:
+        return price * 1.10
+    elif tax_type == 4:
+        return price * 1.20
+    else:
+        return price
 
+print(compute_tax(100,1))
+print(compute_tax(100,2))
+print(compute_tax(100,3))
+print(compute_tax(100,4))
+print(compute_tax(100,99))
+    
